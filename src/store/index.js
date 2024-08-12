bf7e4c9fd9f81a15e6c780802d724db9252a2621
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-08-12 20:27:56 +0530

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,46 @@
+// import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { counterSlice } from "../reducers/counterReducer";
+import { authSlice } from "../reducers/authReducer";
+
+const store = configureStore({
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+});
+
+
+
+export default store;
+
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === "increment") {
+//     return {
+//       counter: state.counter + 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+//   if (action.type === "increase") {
+//     return {
+//       counter: state.counter + action.amount,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "decrement") {
+//     return {
+//       counter: state.counter - 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+//   if (action.type === "setToDefault") {
+//     return {
+//       counter: state.counter,
+//       showCounter: !state.showCounter,
+//     };
+//   }
+
+//   return state;
+// };
+
+// const store = createStore(counterReducer);
+
+//export default store;
